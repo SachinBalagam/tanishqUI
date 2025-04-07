d0ee762f7f20716dd0acbf2705e8282b143a8416
shiva prasad m <shivaprasadmakela@gmail.com>
2025-04-07 22:47:42 +0530

    added initial route and basic home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import styles from "../styles/Home.module.css";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className={styles.header}>Home Page</h1>
+    </div>
+  );
+}
+export default Home;
