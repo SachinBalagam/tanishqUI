a75fbd4daad0046d82e7eb1a0a7fdcb42442a778
Venkataraochavala <c.venkatarao@fincity.com>
2025-04-08 00:21:36 +0530

    add Bootstrap and Sass dependencies; update Header component layout

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -3,9 +3,24 @@ import styles from "../../styles/home/Header.module.css";
 const Header = () => {
   return (
     <div className={styles.header}>
-      <div>
-        <img src="../../src/assets/header/download.svg" alt="logo" className={styles.logo} />
-      </div>
+      <nav>
+        <div className='container'>
+          <div className="topnav-bar">
+            <div className="logo-container">
+              <a href="https://www.tanishq.co.in/">
+              <div class="custom-img">
+                <img src="https://www.tanishq.co.in/assets/images/logo.svg" alt="Tanishq Logo" className="logo-image d-none display-lg" width="80px;" />
+                <img src="https://www.tanishq.co.in/assets/images/logo.svg" alt="Tanishq Logo" className="default-image d-block display-lg-none" width="40px;" />
+              
+              </div>
+              </a>
+            </div>
+            <div className="search-contaiuner">
+            </div>
+            <div class="right-top-section"></div>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 };
