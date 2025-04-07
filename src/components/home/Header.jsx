fe9b2440cfa90497ce5fc30764674a52373fd0ea
shiva prasad m <shivaprasadmakela@gmail.com>
2025-04-07 23:16:37 +0530

    added sample header

---
+++ src/components/home/Header.jsx
@@ -0,0 +1,11 @@
+import styles from "../../styles/home/Header.module.css";
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <h1>Welcome to the Header</h1>
+    </div>
+  );
+};
+
+export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
-import styles from "../styles/Home.module.css";
+import Header from "../components/home/Header.jsx";
 
 const Home = () => {
   return (
     <div>
-      <h1 className={styles.header}>Home Page</h1>
+      <Header />
     </div>
   );
 }
